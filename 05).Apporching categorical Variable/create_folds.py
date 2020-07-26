f485e52be398ab1a71c8f5b71fd7ddecc8565b36
Shubhangi Dabral <shubhidabral@gmail.com>
2020-07-26 20:44:44 +0530

    Add files via upload

---
+++ 05).Apporching categorical Variable/create_folds.py
@@ -0,0 +1,31 @@
+#create_folds.py
+#import pandas and model_selection module of scikit-learn
+
+import pandas as  pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+
+    # Read training data
+    df = pd.read_csv("train.csv")
+
+    # we create a new column called kfold and fill it with -1
+    df["kfold"] = -1
+
+    # the necxt step id to randomize the rows of the data
+    df = df.sample(frac= 1).reset_index(drop = True)
+
+    # fetch labels
+    y = df.target.values
+
+    #initialze the kfold class from model_selection module
+    kf = model_selection.StratifiedKFold(n_splits = 5)
+
+    #fill the new kfold column
+
+    for f, (t_,v_) in enumerate(kf.split(X = df, y = y)):
+        df.loc[v_,"kfold"] = f
+
+    
+    #save the new csv with kfold column
+    df.to_csv("train_folds.csv",index = False)
